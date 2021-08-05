c1d652944d2d5eae281b6562aea69ae216b06fa9
jazper298 <minatosjutsu@gmail.com>
2021-08-06 00:04:27 +0800

    partial video library

--- lib/modules/videolab/videolab_controller.dart
+++ lib/modules/videolab/videolab_controller.dart
@@ -1,3 +1,71 @@
+import 'package:chewie/chewie.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:video_player/video_player.dart';
 
-class VideolabController extends GetxController {}
+class VideolabController extends GetxController {
+  final scrollController = TrackingScrollController();
+  late VideoPlayerController videoPlayerController;
+  ChewieController? chewieController;
+  RxList<VideoLab> videoList = <VideoLab>[
+    VideoLab(
+      title: 'Tony Atillo Solo',
+      video: 'assets/images/solo.mp4',
+    ),
+    VideoLab(
+      title: 'TMarty Friedman Solo',
+      video: 'assets/images/solo.mp4',
+    ),
+    VideoLab(
+      title: 'John Petrucci Solo',
+      video: 'assets/images/solo.mp4',
+    ),
+  ].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    initializedPlayer();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+    videoPlayerController.dispose();
+    chewieController!.dispose();
+  }
+
+  Future<void> initializedPlayer() async {
+    videoPlayerController =
+        VideoPlayerController.asset('${videoList[0].video}');
+    await Future.wait([videoPlayerController.initialize()]);
+    chewieController = ChewieController(
+      videoPlayerController: videoPlayerController,
+      autoPlay: true,
+      looping: true,
+      materialProgressColors: ChewieProgressColors(
+        playedColor: Colors.red,
+        handleColor: Colors.cyanAccent,
+        backgroundColor: Colors.yellow,
+        bufferedColor: Colors.lightGreen,
+      ),
+      placeholder: Container(
+        color: Colors.greenAccent,
+      ),
+      autoInitialize: true,
+    );
+    update();
+  }
+}
+
+class VideoLab {
+  String? title;
+  String? video;
+
+  VideoLab({this.title, this.video});
+}
--- lib/modules/videolab/videolab_view.dart
+++ lib/modules/videolab/videolab_view.dart
@@ -1,14 +1,74 @@
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:video_player/video_player.dart';
+import 'package:yes_premium/modules/videolab/videolab_controller.dart';
+import 'package:sizer/sizer.dart';
 
 class VideoLabView extends StatelessWidget {
   const VideoLabView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Video Lab'),
-      ),
+    final controller = Get.put(VideolabController());
+    return CustomScrollView(
+      controller: controller.scrollController,
+      slivers: [
+        SliverAppBar(
+          brightness: Brightness.light,
+          backgroundColor: Colors.white,
+          leading: Container(
+            margin: EdgeInsets.only(top: 7.0, bottom: 7.0),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.grey.shade300,
+            ),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.close,
+                size: 30,
+              ),
+            ),
+          ),
+          title: Text(
+            'Videos',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.sp),
+          ),
+          centerTitle: true,
+          floating: true,
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              final VideoLab post = controller.videoList[index];
+              return Card(
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical: 5.0),
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                  child: controller.chewieController != null &&
+                          controller.chewieController!.videoPlayerController
+                              .value.isInitialized
+                      ? Chewie(
+                          controller: controller.chewieController!,
+                        )
+                      : Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            CircularProgressIndicator(),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Text('Loading')
+                          ],
+                        ),
+                ),
+              );
+            },
+            childCount: controller.videoList.length,
+          ),
+        ),
+      ],
     );
   }
 }
