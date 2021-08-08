6bb99bc664aa907b7770d4e0389af50cf2a898eb
jazper298 <minatosjutsu@gmail.com>
2021-08-08 19:52:55 +0800

    notification list views and dummy data

--- lib/modules/notification/notification_controller.dart
+++ lib/modules/notification/notification_controller.dart
@@ -1,6 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:html/parser.dart';
 
 class NotificationController extends GetxController {
   final scrollController = TrackingScrollController();
+  var isLoading = true.obs;
+  @override
+  void onInit() {
+    super.onInit();
+    if (!isLoading.value) isLoading(true);
+  }
+
+  RxList<Notifications> notificationList = <Notifications>[
+    Notifications(
+      image: 'assets/images/mina.jpg',
+      content: 'You Shared a PDF file to Kirk Hammett.',
+      time: 'Mon at 4:00PM',
+    ),
+    Notifications(
+      image: 'assets/images/sana.jpg',
+      content: 'Kim Tae Hee Shared a Word file to you.',
+      time: 'Tue at 2:45PM',
+    ),
+    Notifications(
+      image: 'assets/images/cute.jpg',
+      content: 'You Shared a Excel file to Marty Friedman.',
+      time: 'Wed at 9:10AM',
+    ),
+    Notifications(
+      image: 'assets/images/nayeon.jpg',
+      content: 'Im Nayeon Shared a PDF file to you.',
+      time: 'Thu at 7:20PM',
+    ),
+    Notifications(
+      image: 'assets/images/sherry.jpg',
+      content: 'You Shared a Word file to Dave Msutaine.',
+      time: 'Fri at 3:50PM',
+    ),
+    Notifications(
+      image: 'assets/images/soo.jpg',
+      content: 'Soo In Lee Shared a POWERPOINT file to you.',
+      time: 'Sat at 8:15AM',
+    ),
+  ].obs;
+
+  String parseHtmlString(String htmlString) {
+    final document = parse(htmlString);
+    final String parsedString =
+        parse(document.body!.text).documentElement!.text;
+
+    return parsedString;
+  }
+}
+
+class Notifications {
+  String? image;
+  String? content;
+  String? time;
+
+  Notifications({this.image, this.content, this.time});
 }
--- lib/modules/notification/notification_view.dart
+++ lib/modules/notification/notification_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:yes_premium/colors.dart';
 import 'package:yes_premium/modules/notification/notification_controller.dart';
 import 'package:yes_premium/routes/app_routes.dart';
 import 'package:sizer/sizer.dart';
@@ -38,6 +39,97 @@ class NotificationView extends StatelessWidget {
               ),
             ),
           ],
+        ),
+        SliverToBoxAdapter(
+          // final VideoLab post = controller.videoList[index];
+          child: Obx(
+            () => controller.isLoading.value &&
+                    controller.notificationList.length <= 0
+                ? Container(
+                    height: 30.h,
+                    color: Colors.grey[200],
+                  )
+                : ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: controller.notificationList.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: Container(
+                          margin: EdgeInsets.symmetric(vertical: 5.0),
+                          padding: EdgeInsets.symmetric(vertical: 8.0),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 12.0),
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  children: [
+                                    //Post Header
+                                    Row(
+                                      children: [
+                                        ClipOval(
+                                          child: Image.asset(
+                                            '${controller.notificationList[index].image}',
+                                            height: 40.0,
+                                            width: 40.0,
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          width: 8.0,
+                                        ),
+                                        Expanded(
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                '${controller.notificationList[index].content}',
+                                                style: TextStyle(
+                                                  fontSize: 11.sp,
+                                                  fontWeight: FontWeight.w600,
+                                                ),
+                                              ),
+                                              Row(
+                                                children: [
+                                                  Text(
+                                                    '${controller.notificationList[index].time}  •  ',
+                                                    style: TextStyle(
+                                                      color: Colors.grey[600],
+                                                      fontSize: 9.sp,
+                                                    ),
+                                                  ),
+                                                  Icon(
+                                                    Icons.public,
+                                                    color: Colors.grey[400],
+                                                    size: 12.0,
+                                                  ),
+                                                ],
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                        IconButton(
+                                          icon: const Icon(Icons.more_horiz),
+                                          onPressed: () => {
+                                            print(
+                                                '${controller.notificationList.length}'),
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+          ),
         ),
       ],
     );
